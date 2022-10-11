83fabcb690ed0a40abd20cbcd3020d8d2ed9516c
cmolinier <camille.molinier@etudiant.univ-rennes1.fr>
2022-10-11 12:34:37 +0200

    complete command loader

--- V2/srcV2/src/implementations/Command/Commands/Load.java
+++ V2/srcV2/src/implementations/Command/Commands/Load.java
@@ -9,6 +9,7 @@ import implementations.Memento.MyOriginator;
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
@@ -21,6 +22,8 @@ public class Load implements Command {
   private String preString;
   private String postString;
   private String script;
+  private int position;
+  private Command command;
 
   /****************************************************************************************************/
   /*                                            Constructor                                           */
@@ -29,6 +32,7 @@ public class Load implements Command {
     preString = content.substring(0, pos);
     postString = content.substring(pos, content.length());
     script = scriptName;
+    position = pos;
 
     receiver = new Engine();
     originator = new MyOriginator();
@@ -43,11 +47,52 @@ public class Load implements Command {
       Scanner sc = new Scanner(file);
 
       while (sc.hasNextLine()) {
-        receiver.delete(0, SimpleBuffer.getInstance().getContent().length());
-        receiver.insert(sc.nextLine(), 0);
+        summonCommand(sc.nextLine());
       }
     } catch (Exception e) {System.out.println(e);}
-    //originator.save(SimpleBuffer.getInstance().getContent(),
-     //   new ArrayList<String>(Arrays.asList("Load", start+"", stop+"")));
+    originator.save(SimpleBuffer.getInstance().getContent(),
+      new ArrayList<String>(Arrays.asList("Load", preString+postString, position+"", script)));
+  }
+
+  private void summonCommand(String line){
+    List<List<String>> lineList = getLineList(line);
+
+    for(List<String> stringLine : lineList) {
+      switch (stringLine.get(0)) {
+        case "Copy" :
+          command = new Copy(Integer.parseInt(stringLine.get(1)), Integer.parseInt(stringLine.get(2)));
+          break;
+        case "Cut" :
+          command = new Cut(Integer.parseInt(stringLine.get(1)), Integer.parseInt(stringLine.get(2)));
+          break;
+        case "Delete" :
+          command = new Delete(Integer.parseInt(stringLine.get(1)));
+          break;
+        case "Insert" :
+          command = new Insert(stringLine.get(1), Integer.parseInt(stringLine.get(2)));
+          break;
+        case "Paste" :
+          command = new Paste(Integer.parseInt(stringLine.get(1)));
+          break;
+        case "Redo" :
+          command = new Redo();
+          break;
+        case "Replace" :
+          command = new Replace(Integer.parseInt(stringLine.get(1)), Integer.parseInt(stringLine.get(2)));
+          break;
+      }
+      command.execute();
+    }
+  }
+
+  private List<List<String>> getLineList(String line){
+    String content = line.substring(1, line.length()-1);
+    List<String> splited = Arrays.asList(content.split("\n"));
+
+    List<List<String>> lineList = new ArrayList<List<String>>();
+    for (String s : splited) {
+      lineList.add(Arrays.asList(s.split(", ")));
+    }
+    return lineList;
   }
 }
